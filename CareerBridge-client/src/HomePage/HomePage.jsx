6102f877a32b71ea80c788a7565d757c79bc141d
parthasarathi019 <parthasarathaie@gmail.com>
2023-08-14 13:09:19 +0600

    App.jsx Deleted

--- CareerBridge-client/src/HomePage/HomePage.jsx
+++ CareerBridge-client/src/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Container from '../Components/Container/Container'
-import App from '../App'
 
 const HomePage = () => {
   return (
     <>
       <Container>
-        <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Page Components 😃</div>
-        <App></App>
+        <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Header Page Components 😃</div>
       </Container>
       
     </>
