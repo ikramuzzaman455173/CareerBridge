91585f7cc37e96f89e18e41bb7b399257f9ae949
parthasarathi019 <parthasarathaie@gmail.com>
2023-08-14 13:06:26 +0600

    Delete app.jsx file

--- CareerBridge-client/src/HomePage/HomePage.jsx
+++ CareerBridge-client/src/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from '../Components/Container/Container'
+import App from '../App'
 
 const HomePage = () => {
   return (
     <>
       <Container>
         <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Page Components 😃</div>
+        <App></App>
       </Container>
+      
     </>
   )
 }
