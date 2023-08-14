372be99f423cf6c6e81ab6b90c73d22e66baa675
sajim-khan <shossain1427@gmail.com>
2023-08-14 12:53:37 +0600

    added header

--- CareerBridge-client/src/App.jsx
+++ CareerBridge-client/src/App.jsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
 		<>
-			<div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Components 😃</div>
+			<div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Header Components 😃</div>
     </>
   )
 }
