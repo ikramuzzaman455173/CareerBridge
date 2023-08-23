472eee10d34a933cfb3e2e902b6183b1de536b78
antar <antumojumder15@gmail.com>
2023-08-23 11:10:33 -0700

    blogpage four card item added & added blogpage route

--- CareerBridge-client/src/Routes/Routes.jsx
+++ CareerBridge-client/src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainLagoutPage from '../MainLagoutPage/MainLagoutPage'
 import HomePage from '../HomePage/HomePage'
+import BlogPage from '../BlogPage/BlogPage'
+import BlogDetails from '../BlogPage/BlogDetails/BlogDetails'
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
         path: '/',
         element: <HomePage />
       },
+      {
+        path: '/blog',
+        element: <BlogPage/>
+      },
+      {
+        path: 'blog-details',
+        element:<BlogDetails/>
+      }
       // {
       //   path: '/login',
       //   element: <Login />
